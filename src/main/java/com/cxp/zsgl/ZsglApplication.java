66e3874fd54c4139e68368e40639fa1277f31d98
陈晓鹏 <1111@qq.com>
2021-04-02 09:53:01 +0800

    我的代买

---
+++ src/main/java/com/cxp/zsgl/ZsglApplication.java
@@ -0,0 +1,13 @@
+package com.cxp.zsgl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZsglApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZsglApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cxp/zsgl/ZsglApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cxp.zsgl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZsglApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
